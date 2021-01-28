4362134fdc0c98e798b5532e91e4c7085b5e1749
aaazlkm <40765910+aaazlkm@users.noreply.github.com>
2021-01-28 21:45:08 +0900

    add example project

---
+++ example/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
