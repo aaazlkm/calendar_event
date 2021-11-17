f5eb6b4cf9674588768e6801022d8a9d2bdfac33
aaazlkm <40765910+aaazlkm@users.noreply.github.com>
2021-11-17 10:32:58 +0900

    remove dateTime.add(Duration)

--- lib/src/factory/calendar_days.dart
+++ lib/src/factory/calendar_days.dart
@@ -11,7 +11,7 @@ List<Day> createCalendarDays({
   final startDateInMonth = DateTime(yearMonth.year, yearMonth.month);
   final endDateInMonth = DateTime(yearMonth.year, yearMonth.month + 1, 0);
   final offset = startDayOfWeek.calculateOffset(to: DayOfWeekEx.from(dateTime: startDateInMonth));
-  final startDayInCalendar = startDateInMonth.add(Duration(days: -offset));
+  final startDayInCalendar = startDateInMonth.addAmount(dayAmount: -offset);
   final weeksPerCalendar = endDateInMonth.difference(startDayInCalendar).inDays ~/ DateTime.daysPerWeek + 1;
   final calendarDates = List.generate(weeksPerCalendar * DateTime.daysPerWeek, (i) => i).map(
     (i) {
